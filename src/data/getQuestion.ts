a15a7eee54d02f2b86f4202201f24c2ea65913b0
Michał Anioł <michal.janusz.aniol@gmail.com>
2026-05-07 15:38:19 +0200

    questions

--- src/data/getQuestion.ts
+++ src/data/getQuestion.ts
@@ -1,3 +1,27 @@
 namespace data {
-    export const getQuestion = () => {}
+    export const getQuestion = () => {
+        /**
+ * @swagger
+ * /rol04/api/get-questions:
+ *   get:
+ *     tags:
+ *       - data
+ *     summary: Zwraca all-questions
+ *     description: Zwraca all-questions
+ *     responses:
+ *       200:
+ *         description: Wysłano
+ */
+
+
+        core.app.get(url.data.questions, core.csrfProtection, async (req: RequestT, res: ResponseT) => {
+            const verification = utils.jwtToken.verify(req, res)
+
+            if (verification) {
+                const config = await fm.loadData('all-questions')
+
+                res.json(config)
+            }
+        })
+    }
 }
